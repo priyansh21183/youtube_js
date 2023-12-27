b76a755ff03c3544a4d629d5ed6ccd784c2442ab
powe_rpixel_21 <108052479+priyansh21183@users.noreply.github.com>
2023-12-27 21:23:05 +0000

    conversion_dataType

---
+++ 01_basics/conversion_of_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"
+let a=undefined
+let b= Boolean(a)
+let c= String(b)
+
+let d=Number(a);
+let e=undefined;
+
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+console.log(d);
+console.log(e);
+
+
+
