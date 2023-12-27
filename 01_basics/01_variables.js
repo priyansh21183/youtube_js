f10f51862174fb033a70f2ea1ea2587441eac3fa
powe_rpixel_21 <108052479+priyansh21183@users.noreply.github.com>
2023-12-27 20:40:58 +0000

    data_types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "kumarpriyansh844@google.com"
+var accountPassword ="123456"
+accountCity= "Delhi"
+
+
+//accountId=2
+accountEmail = "kumarpriyansh@google.com"
+accountPassword ="444"
+accountCity="BIhar"
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
