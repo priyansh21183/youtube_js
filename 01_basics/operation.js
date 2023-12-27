2f4c4b9837f26c026a4c45b8fc32e307e3af1704
powe_rpixel_21 <108052479+priyansh21183@users.noreply.github.com>
2023-12-27 21:47:23 +0000

    Operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,24 @@
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+console.log(x,y,x2,y2);
+a= "happy"
+b= " new"
+c= " year2024"
+d=a+b+c
+console.log(d)
+
+console.log("1"+2+3);
+console.log(1+2+"3");
+console.log(1+"2"+3);
+console.log(-true);
+console.log("a"+1);
+
+let z,m,k;
+z=m=k=9;
+console.log(z,m,k)
