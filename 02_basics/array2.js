8b598558d9ca00476d09a5ad3bfc63e3953637e9
powe_rpixel_21 <108052479+priyansh21183@users.noreply.github.com>
2024-01-02 08:52:34 +0000

    arr

---
+++ 02_basics/array2.js
@@ -0,0 +1,8 @@
+const marvelHeroes=["Thor","IronMan","Spiderman","DoctorStrange"]
+const dcHeroes=["Superman","WonderWomen","Batman","Flash"]
+
+//marvelHeroes.push(dcHeroes)
+//console.log(marvelHeroes);
+
+const all=marvelHeroes.concat(dcHeroes)
+console.log();
